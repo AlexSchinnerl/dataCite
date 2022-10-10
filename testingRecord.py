aa73434f45ed8ce6faad417406323084285f6696
Alex Schinnerl <alex.itsme@gmx.at>
2022-10-10 17:00:52 +0200

    record test

--- testingRecord.py
+++ testingRecord.py
@@ -5,32 +5,72 @@ def write_log(record, text):
         logFile.write("{} - {}\n".format(acNr, text))
 
 
+def subfield_check(datafield, subfieldCode, record):
+    missingSubfield = False
+    checkingSubfield = record.findall(f".//datafield[@tag='{datafield}']")
+    # txtMsg = ""
+    for item in checkingSubfield:
+        if item.find(f"subfield[@code='{subfieldCode}']") is None:
+            txtMsg = f"Missing Subfield '{subfieldCode}' in datafield {datafield}"
+            write_log(record, txtMsg)
+            missingSubfield = True
+    return missingSubfield
+
+
 def check_subfields(datafield, subfieldcode, datafieldtag):
     missingSubfield = False
     textMsg = ""
-    if datafield.find(f"subfield[@code='{subfieldcode}']") == None:
+    if datafield.find(f"subfield[@code='{subfieldcode}']") is None:
         textMsg = f"Missing Subfield '{subfieldcode}' in {datafieldtag}"
         missingSubfield = True
     return missingSubfield, textMsg
+
+
+# check 024
+
+
+
+
+
+def check_for_doi(record):
+    checkDOI = False
+    for item in record.findall(".//datafield[@tag='024']"):
+        if item.find("subfield[@code='2']").text == "doi":
+                checkDOI = True
+    if checkDOI == False:
+        textMsg = "No DOI in record"
+        # print(textMsg)
+        write_log(record, textMsg)
+
 
 def check_mandatory_fields(record):
     acNr = record.find(".//controlfield[@tag='009']").text
     print(f"Processing: {acNr}")
     # check for DOI:
-    check_for_doi = False
     for item in record.findall(".//datafield[@tag='024']"):
-        subfield_check, textMsg = check_subfields(datafield=item, subfieldcode="2", datafieldtag="024")
-        if subfield_check == True:
-            write_log(record, textMsg)    
-        else:
-            # checks if doi is in any 024
-            if item.find("subfield[@code='2']").text == "doi":
-                check_for_doi = True
+        if subfield_check("024", "2", record) == False:
+            check_for_doi(record)
+        
 
-    if check_for_doi == False:
-        textMsg = "No DOI in record"
-        # print(textMsg)
-        write_log(record, textMsg)
+
+
+    # check_for_doi = False
+    # for item in record.findall(".//datafield[@tag='024']"):
+    #     subfield_check, textMsg = check_subfields(datafield=item, subfieldcode="2", datafieldtag="024")
+    #     if subfield_check == True:
+    #         write_log(record, textMsg)    
+    #     else:
+    #         # checks if doi is in any 024
+    #         if item.find("subfield[@code='2']").text == "doi":
+    #             check_for_doi = True
+
+    # if check_for_doi == False:
+    #     textMsg = "No DOI in record"
+    #     # print(textMsg)
+    #     write_log(record, textMsg)
+
+
+
     # check if author in 100 and/or 700
     if record.find(".//datafield[@tag='100']") == None and record.find(".//datafield[@tag='700']") == None:
         textMsg = "No author (datafield 100 or datafield 700) in record"
